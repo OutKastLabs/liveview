32fbc409a9b148a58d53fa035cd9a28e90895a94
siddhant3030 <ssiddhant3030@gmail.com>
2020-05-26 01:51:45 +0530

    add the 0.13.0 deps for liveview

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Reactor.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_live_view, "~> 0.13.0"}
     ]
   end
 
