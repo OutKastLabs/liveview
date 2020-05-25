d65b9b9edb8d100699ab56d2562c3d9070559013
siddhant3030 <ssiddhant3030@gmail.com>
2020-05-26 01:45:12 +0530

    phoenix liveview project

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :reactor,
+  ecto_repos: [Reactor.Repo]
+
+# Configures the endpoint
+config :reactor, ReactorWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "5IzxKTWc7pV/bOTiJ2UdmV9T0qH5a+/pGBR24gPOb32JC0bTpqtBxRC/6slUW51H",
+  render_errors: [view: ReactorWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Reactor.PubSub,
+  live_view: [signing_salt: "dlm/6TP0"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
