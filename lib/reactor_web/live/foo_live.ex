c30b27ad6a26e19cd125c5d57cb7a857e50b437c
siddhant3030 <ssiddhant3030@gmail.com>
2020-05-29 16:37:05 +0530

    mount function accept 3 arguments now

--- lib/reactor_web/live/foo_live.ex
+++ lib/reactor_web/live/foo_live.ex
@@ -1,13 +1,14 @@
 defmodule ReactorWeb.FooLive do
   use Phoenix.LiveView
 
-  def mount(_session, socket) do
-    {:ok, socket}
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, key: "siddhant")}
   end
 
   def render(assigns) do
     ~L"""
     <h1>Hi!</h1>
+    <%= @key %>
     """
   end
 end
