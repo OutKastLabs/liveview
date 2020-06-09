236b6aeb9c7cdccc7b4cc26fbd91a4b84fc07f22
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-10 01:40:04 +0530

    adding string

--- lib/reactor/coordinate.ex
+++ lib/reactor/coordinate.ex
@@ -46,4 +46,8 @@ defmodule Reactor.Coordinate do
   def set_in_island(coordinate, value) when is_atom value do
     Agent.update(coordinate, fn state -> Map.put(state, :in_island, value) end)
   end
+
+  def to_string(coordinate) do
+  "(in_island:#{island(coordinate)}, guessed:#{guessed?(coordinate)})"
+  end
 end
