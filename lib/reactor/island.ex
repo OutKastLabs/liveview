bc734084a9a18f3ab11b8e2e7a66cc3f84e889b2
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-11 01:07:20 +0530

    new function added for forested

--- lib/reactor/island.ex
+++ lib/reactor/island.ex
@@ -1,12 +1,17 @@
 defmodule Reactor.Island do
   alias __MODULE__
+  alias Reactor.Coordinate
 
   def start_link() do
    Agent.start_link(fn -> [] end)
   end
 
-
   def replace_coordinates(island, new_coordinates) when is_list new_coordinates do
-    Agent.update(island, fn _state -> new_coordinates end)
+   Agent.update(island, fn _state -> new_coordinates end)
+  end
+
+  def forested?(island) do island
+    |> Agent.get(fn state -> state end)
+    |> Enum.all?(fn coord -> Coordinate.hit?(coord) end)
   end
 end
