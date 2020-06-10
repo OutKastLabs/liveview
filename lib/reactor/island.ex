e3c1dceb2590ecf914e9969e88b8305b1d108fe5
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-10 20:49:38 +0530

    replace coordinates

--- lib/reactor/island.ex
+++ lib/reactor/island.ex
@@ -4,4 +4,9 @@ defmodule Reactor.Island do
   def start_link() do
    Agent.start_link(fn -> [] end)
   end
+
+
+  def replace_coordinates(island, new_coordinates) when is_list new_coordinates do
+    Agent.update(island, fn _state -> new_coordinates end)
+  end
 end
