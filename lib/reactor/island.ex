8ce714c59a2014cfe53ce8678a46ed8fb713dda9
siddhant3030 <ssiddhant3030@gmail.com>
2020-06-10 16:25:07 +0530

    deps added

--- lib/reactor/island.ex
+++ lib/reactor/island.ex
@@ -1,4 +1,7 @@
 defmodule Reactor.Island do
   alias __MODULE__
 
+  def start_link() do
+   Agent.start_link(fn -> [] end)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Reactor.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, "~> 0.13.0"}
+      {:phoenix_live_view, "~> 0.13.0"},
+      {:git_hooks, "~> 0.4.0", only: [:test, :dev], runtime: false}
     ]
   end
 
