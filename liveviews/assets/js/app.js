6678d4269002c04a118753def7881e623ded1554
siddhant3030 <ssiddhant3030@gmail.com>
2020-08-18 19:18:52 +0530

    livesocket is not working

--- liveviews/assets/js/app.js
+++ liveviews/assets/js/app.js
@@ -32,6 +32,7 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+console.log(liveSocket)
 
 
 
