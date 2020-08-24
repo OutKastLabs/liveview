87ce5285946371d2e39e0ac752ade5e36642b810
siddhant3030 <ssiddhant3030@gmail.com>
2020-08-24 16:35:28 +0530

    post component

--- twit/lib/twit_web/live/post_live/post_component.ex
+++ twit/lib/twit_web/live/post_live/post_component.ex
@@ -1,3 +1,34 @@
 defmodule TwitWeb.PostLive.PostComponent do
    use TwitWeb, :live_component
+   
+   def render(assigns) do
+     <table>
+  <thead>
+    <tr>
+      <th>Username</th>
+      <th>Body</th>
+      <th>Likes count</th>
+      <th>Reposts count</th>
+
+      <th></th>
+    </tr>
+  </thead>
+  <tbody id="posts">
+    <%= for post <- @posts do %>
+      <tr id="post-<%= post.id %>">
+        <td><%= post.username %></td>
+        <td><%= post.body %></td>
+        <td><%= post.likes_count %></td>
+        <td><%= post.reposts_count %></td>
+
+        <td>
+          <span><%= live_redirect "Show", to: Routes.post_show_path(@socket, :show, post) %></span>
+          <span><%= live_patch "Edit", to: Routes.post_index_path(@socket, :edit, post) %></span>
+          <span><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: post.id, data: [confirm: "Are you sure?"] %></span>
+        </td>
+      </tr>
+    <% end %>
+  </tbody>
+</table>
+   end
 end
